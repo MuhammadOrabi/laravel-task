2cc7636bc427ea96a3fbcbc8c6715a54a5c7bd62
Muhammad Orabi <muhammad.orabi.2014@hotmail.com>
2018-10-07 08:18:02 +0200

    fix issue and add testing

--- app/Observers/ProposalOpserver.php
+++ app/Observers/ProposalOpserver.php
@@ -15,6 +15,28 @@ class ProposalObserver
      */
     public function created(Proposal $proposal)
     {
+        $this->generateCode($proposal);
+    }
+
+    /**
+     * Handle the Proposal "saved" event.
+     *
+     * @param  \App\Models\Proposal  $proposal
+     * @return void
+     */
+    public function saved(Proposal $proposal)
+    {
+        $this->generateCode($proposal);        
+    }
+
+    /**
+     * Handle the Proposal code generating.
+     *
+     * @param  \App\Models\Proposal  $proposal
+     * @return void
+     */
+    public function generateCode(Proposal $proposal)
+    {    
         // Saveing the code attribute
         $type = uperFirstLetter($proposal->type);
         $approval_from = uperFirstLetter($proposal->approval_from);
--- tests/UserTest.php
+++ tests/UserTest.php
@@ -2,20 +2,52 @@
 
 use Laravel\Lumen\Testing\DatabaseMigrations;
 use Laravel\Lumen\Testing\DatabaseTransactions;
+use Illuminate\Support\Facades\Artisan;
+use App\Models\User;
+use Firebase\JWT\JWT;
 
 class UserTest extends TestCase
 {
+    use DatabaseMigrations;
+    protected $user;
+    protected $token;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        Artisan::call('migrate:fresh', ['--seed' => true]);
+        $this->user = User::whereEmail('ahmed@app.com')->first();
+        $payload = [
+            'iss' => 'lumen-jwt-test',
+            'sub' => $this->user->id,
+            'iat' => time(),
+            'exp' => time() + 60*60*60
+        ];
+        
+        $this->token = JWT::encode($payload, env('JWT_SECRET'));    
+    }
+
     /**
-     * A basic test example.
+     * Test Create Proposal.
      *
      * @return void
      */
-    public function testExample()
+    public function testCreateProposal()
     {
-        $this->get('/');
+        $data = [
+            'type' => 'web development',
+            'approval_from' => 'Mohamed Ansary',
+            'client_source' => 'Digital campaign',
+            'client_name' => 'Orabi',
+            'value' => 'blah blah',
+            'due' => '2018-10-05 17:32:11'
+        ];
 
-        $this->assertEquals(
-            $this->app->version(), $this->response->getContent()
-        );
+        $response = $this->post('/api/proposal', $data, [
+            'Authorization' => 'Bearer '. $this->token
+        ]);
+        
+        $this->assertOk();
     }
 }
